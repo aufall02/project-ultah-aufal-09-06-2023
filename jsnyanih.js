989ebc85b720a3464bfb638cc5ac11c2d3790f44
aufall02 <aufaluzumaki@gmail.com>
2023-06-09 22:09:07 +0700

    first commit

---
+++ jsnyanih.js
@@ -0,0 +1,77 @@
+document.getElementById("button").addEventListener("click", function() {
+  var alertBox = document.getElementById("button");
+  alertBox.style.display = "none";
+  var alertBox = document.getElementById("alert");
+  alertBox.style.display = "block";
+});
+
+document.getElementById("Button").addEventListener("click", function() {
+  var alertBox = document.getElementById("alert");
+  alertBox.style.display = "none";
+  var alertBox = document.getElementById("alert2");
+  alertBox.style.display = "block";
+});
+
+document.getElementById("Button2").addEventListener("click", function() {
+  var alertBox = document.getElementById("alert2");
+  alertBox.style.display = "none";
+  var alertBox = document.getElementById("alert3");
+  alertBox.style.display = "block";
+});
+
+document.getElementById("Button3").addEventListener("click", function() {
+  var alertBox = document.getElementById("alert3");
+  alertBox.style.display = "none";
+  var alertBox = document.getElementById("alert4");
+  alertBox.style.display = "block";
+});
+
+document.getElementById("Button4").addEventListener("click", function() {
+  var alertBox = document.getElementById("alert4");
+  alertBox.style.display = "none";
+  var alertBox = document.getElementById("alert5");
+  alertBox.style.display = "block";
+});
+
+document.getElementById("Button5").addEventListener("click", function() {
+  var alertBox = document.getElementById("alert5");
+  alertBox.style.display = "none";
+  var alertBox = document.getElementById("alert6");
+  alertBox.style.display = "block";
+});
+
+document.getElementById("Button6").addEventListener("click", function() {
+  var alertBox = document.getElementById("alert6");
+  alertBox.style.display = "none";
+  var alertBox = document.getElementById("alert8");
+  alertBox.style.display = "block";
+});
+
+document.getElementById("Button8").addEventListener("click", function() {
+  var alertBox = document.getElementById("alert8");
+  alertBox.style.display = "none";
+  var alertBox = document.getElementById("alert9");
+  alertBox.style.display = "block";
+});
+
+document.getElementById("Button9").addEventListener("click", function() {
+  var alertBox = document.getElementById("alert9");
+  alertBox.style.display = "none";
+  var alertBox = document.getElementById("alert10");
+  alertBox.style.display = "block";
+});
+
+document.getElementById("Button10").addEventListener("click", function() {
+  var alertBox = document.getElementById("alert10");
+  alertBox.style.display = "none";
+  var alertBox = document.getElementById("alert11");
+  alertBox.style.display = "block";
+});
+
+document.getElementById("Button11").addEventListener("click", function() {
+  var alertBox = document.getElementById("alert11");
+  alertBox.style.display = "none";
+  var alertBox = document.getElementById("penutup");
+  alertBox.style.display = "block";
+});
+
